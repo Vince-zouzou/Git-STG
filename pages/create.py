9d582f9d4f4bcc82d83b81521ad6fe152ca41913
YSOMAD <65582466+Vince-zouzou@users.noreply.github.com>
2025-05-23 20:38:41 +0800

    Update create.py

--- pages/create.py
+++ pages/create.py
@@ -130,7 +130,8 @@ def render_question_form(info, index):
             current_images = [image]
         for img in current_images:
             if img:
-                st.image(img, caption=MESSAGES[current_language]["uploadedImageCaption"])
+                st.write(img)
+                #st.image(img, caption=MESSAGES[current_language]["uploadedImageCaption"])
 
         convert = st.button(MESSAGES[current_language]["aiWriter"], key=f"convert_{index}")
         text = ''
